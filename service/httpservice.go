4f43f084259b8ce3fe4551df9a84d16303009a37
Alexander Ewetumo <trinoxf@gmail.com>
2018-01-15 07:20:48 +0100

    Revert to http direct write [ci skip]

--- service/httpservice.go
+++ service/httpservice.go
@@ -1,32 +1,21 @@
 package service
 
 import (
-	"bytes"
 	"encoding/json"
 	"log"
 	"net/http"
-	"sync"
-)
-
-var (
-	bufferPool = sync.Pool{New: func() interface{} { return new(bytes.Buffer) }}
 )
 
 // Top5Stats returns a http.HandlerFunc which always responds with the last
 // known top5 words and letters.
 func Top5Stats(top5 *Top5WordLetterStat) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		buffer := bufferPool.Get().(*bytes.Buffer)
-		defer bufferPool.Put(buffer)
-
-		if err := json.NewEncoder(buffer).Encode(top5.Stat()); err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(top5.Stat()); err != nil {
 			log.Printf("http error: failed to write top5 stat for request from %+q: %+s", r.RemoteAddr, err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		buffer.WriteTo(w)
 	}
 }
